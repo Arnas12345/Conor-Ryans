b0deadc669183d32d832bfd01a637620f98993f3
Arnas12345 <arnasjuravicius12345@gmail.com>
2021-03-23 17:36:11 +0000

    Fixed error

--- serverConfig.php
+++ serverConfig.php
@@ -5,13 +5,10 @@
    $DB_DATABASE ='loop';
 
 
-<<<<<<< HEAD
-=======
    // $DB_SERVER = 'sql308.epizy.com';
    // $DB_USERNAME = 'epiz_27975826';
    // $DB_PASSWORD = 'VaFXAV3oWtcrBh';
    // $DB_DATABASE ='epiz_27975826_Loop';
->>>>>>> 54d93e7e35a5ccd46ac3e7f89e5cc5dabf559298
    // function connectToDB() {
    //    $conn = new mysqli('localhost', 'root', '', 'loop');
    //    if ($conn -> connect_error) {
