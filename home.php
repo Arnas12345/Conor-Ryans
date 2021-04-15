5667652a41bd93cab91e7e4aec1359eebd4cedb0
Dylan Kearney <dylank09@gmail.com>
2021-04-15 16:41:01 +0100

    add meta tags to files

--- home.php
+++ home.php
@@ -4,6 +4,8 @@
         <title>Loop : Home</title>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
         <link rel="stylesheet" type="text/css" href="css/home.css?v=<?php echo time(); ?>">
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+        
     </head>
     <script type="text/javascript">
         function loopJob(vacancyID, companyID) {
--- organizationProfile.php
+++ organizationProfile.php
@@ -3,6 +3,8 @@
         <title>Loop : User Profie</title>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
         <link rel="stylesheet" type="text/css" href="css/companyEditProfile.css?v=<?php echo time() ?>">
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+        
     </head>
     <body>
 
